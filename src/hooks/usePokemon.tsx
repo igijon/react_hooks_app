11a27ca11974706bdb56c3dbe2d917908f64770d
Inma Gijón <igijoninf@gmail.com>
2026-01-30 13:22:31 +0100

    Conectar varios CustomHooks

--- src/hooks/usePokemon.tsx
+++ src/hooks/usePokemon.tsx
@@ -1,30 +1,48 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface Pokemon {
-    id: number;
-    name: string;
-    imageUrl: string;
+  id: number;
+  name: string;
+  imageUrl: string;
 }
 
 interface Props {
-    i: number;
+  id: number;
 }
 
-export const usePokemon = ({id}: Props) => {
-    const [pokemon, setPokemon] = useState<Pokemon | null>(null);
-  
-  const getPokemonById = async (id: number) => {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-    const data = await response.json();
-    const newPokemon: Pokemon = {
-      id,
-      name: data.name,
-      imageUrl: data.sprites.front_default,
+export const usePokemon = ({ id }: Props) => {
+  const [pokemon, setPokemon] = useState<Pokemon | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      try {
+        setIsLoading(true);
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        const data = await response.json();
+
+        const newPokemon: Pokemon = {
+          id,
+          name: data.name,
+          imageUrl: data.sprites.front_default,
+        };
+
+        setPokemon(newPokemon);
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
+        console.error("Error fetching pokemon:", error);
+        throw error;
+      }
     };
-    setPokemon(newPokemon);
-}
-    return {
-    //Properties
-    pokemon
-  }
-}
+
+    fetchPokemon();
+  }, [id]);
+
+  return {
+    isLoading,
+    pokemon,
+
+    formattedId: id.toString().padStart(3, "0"),
+  };
+};

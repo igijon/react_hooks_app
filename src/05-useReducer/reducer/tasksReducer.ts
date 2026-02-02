36c7fb0cd4616a7b8de641da2d8b70b0d0b40366
Inma Gijón <igijoninf@gmail.com>
2026-02-02 13:39:39 +0100

    Función Reducer

--- src/05-useReducer/reducer/tasksReducer.ts
+++ src/05-useReducer/reducer/tasksReducer.ts
@@ -12,12 +12,64 @@ interface TaskState {
 }
 
 export type TaskAction =
-  | { type: "ADD_TODO"; payload: Todo}
+  | { type: "ADD_TODO"; payload: string }
   | { type: "TOGGLE_TODO"; payload: number }
   | { type: "DELETE_TODO"; payload: number };
 
 //Un reducer es una función que devuelve siempre un estado nuevo.
 //Es una función pura que resuelve un nuevo estado basado en los argumentos
-export const taskReducer = (state: TaskState, action: TaskAction): TaskState => {
-  return state;
+//El patrón Reducer es agnóstico, no depende de React, se podría implementar en cualquier tecnología
+export const taskReducer = (
+  state: TaskState,
+  action: TaskAction,
+): TaskState => {
+  switch (action.type) {
+    case "ADD_TODO": {
+      const newTodo: Todo = {
+        id: Date.now(),
+        text: action.payload,
+        completed: false,
+      };
+      //! No hacer esto, no hacer mutaciones al estado, siempre un nuevo estado
+      //state.todos.push(newTodo);
+
+      return {
+        ...state,
+        todos: [...state.todos, newTodo],
+        length: state.length + 1,
+        pending: state.pending + 1,
+      };
+    }
+    case "DELETE_TODO": {
+        return {
+            ...state,
+            todos: state.todos.filter(todo => todo.id !== action.payload),
+            length: state.length - 1,
+            completed: state.todos.find(todo => todo.id === action.payload)?.completed ? state.completed -1 : state.completed,
+            pending: state.todos.find(todo => todo.id === action.payload)?.completed ? state.pending : state.pending -1,
+        };
+    }
+    case "TOGGLE_TODO": {
+        const updatedTodos = state.todos.map(todo => {
+            if (todo.id === action.payload) {
+                return {
+                    ...todo,
+                    completed: !todo.completed,
+                };
+            }
+            return todo;
+        });
+        const toggledTodo = state.todos.find(todo => todo.id === action.payload);
+        const isCompleted = toggledTodo ? !toggledTodo.completed : false;
+
+        return {
+            ...state,
+            todos: updatedTodos,
+            completed: isCompleted ? state.completed + 1 : state.completed - 1,
+            pending: isCompleted ? state.pending - 1 : state.pending + 1,
+        };
+    }
+    default:
+      return state;
+  }
 };

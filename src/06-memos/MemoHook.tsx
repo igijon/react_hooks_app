d07353b262bb44a99c8a781c124c98e5712a58ad
Inma Gijón <igijoninf@gmail.com>
2026-02-26 17:18:00 +0100

    UseCallback para memorización de funciones

--- src/06-memos/MemoHook.tsx
+++ src/06-memos/MemoHook.tsx
@@ -1,15 +1,20 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { MySubTitle } from "./ui/MySubTitle";
 import { Mytitle } from "./ui/Mytitle";
 
 export const MemoHook = () => {
   const [title, setTitle] = useState("hola");
   const [subTitle, setSubTitle] = useState("mundo");
+
+  const handleMyAPICall = useCallback(() => {
+    console.log("Llamar a mi API - ", subTitle);
+  }, [subTitle]); //tiene un array de dependencias vacío, por lo que la función se memoiza y no se vuelve a crear en cada renderizado
+
   return (
     <div className="bg-gradient flex flex-col gap-4">
       <h1 className="text-2xl font-thin text-white">MemoApp</h1>
       <Mytitle title={title} />
-      <MySubTitle subTitle={subTitle} />
+      <MySubTitle subTitle={subTitle} callMyAPI={handleMyAPICall}/>
       <button
         className="bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer"
         onClick={() => setTitle("Hello, "+new Date().getTime())}
--- src/06-memos/ui/MySubTitle.tsx
+++ src/06-memos/ui/MySubTitle.tsx
@@ -1,12 +1,13 @@
 interface Props {
   subTitle: string;
+  callMyAPI: () => void;
 }
-export const MySubTitle = ({ subTitle }: Props) => {
+export const MySubTitle = ({ subTitle, callMyAPI }: Props) => {
   console.log('My subtitle re-rendered:', subTitle);
   return (
     <>
-      <h6 className="text-2xl fornt-bold">{subTitle}</h6>
-      <button className="bg-indigo-500 text-white px-2 py-1 rounded-md cursor-pointer">Llamar a función</button>
+      <h6 className="text-2xl font-bold">{subTitle}</h6>
+      <button className="bg-indigo-500 text-white px-2 py-1 rounded-md cursor-pointer" onClick={callMyAPI}>Llamar a función</button>
     </>
   );
 };

296d4e2c8416e475bd24f4a699ab8eb780e96a04
Inma Gijón <igijoninf@gmail.com>
2026-02-27 18:12:25 +0100

    Contexto del usuario - Información global

--- src/09-useContext/context/UserContext.tsx
+++ src/09-useContext/context/UserContext.tsx
@@ -1,5 +1,39 @@
-import { useState, type PropsWithChildren } from "react";
+import { createContext, useState, type PropsWithChildren } from "react";
+import type { User } from "../data/user-mock.data";
+
+type AuthStatus = 'checking' | 'authenticated' | 'not-authenticated';
+
+interface UserContextProps {
+  //State
+  authStatus: AuthStatus;
+  user: User | null;
+
+  //Methods
+  login: (userId:number) => boolean;
+  logout: () => void;
+}
+
+
+export const UserContext = createContext({} as UserContextProps);
 
 export const UserContextProvider = ({ children }: PropsWithChildren) => {
-  return <>{children}</>;
+    
+    const [authStatus, setAuthStatus] = useState<AuthStatus>('checking');
+    const [user, setUser] = useState<User | null>(null);
+    
+    const handleLogin = (userId: number) => {
+        console.log({userId});
+        return true;
+    }
+    
+    const handleLogout = () => {
+        console.log('Logout');
+    }
+
+    return <UserContext value={{
+    authStatus,
+    user,
+    login: handleLogin,
+    logout: handleLogout,
+  }}>{children}</UserContext>;
 };

8bdbf509cf46b40696161885071089b2b531f501
Inma Gijón <igijoninf@gmail.com>
2026-02-27 15:44:09 +0100

    Preparando API + Suspense

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { ClientInformation } from "./08-use-suspense/ClientInformation";
 // import { ScrambleWords } from "./05-useReducer/ScrambleWords";
 // import { MemoHook } from "./06-memos/MemoHook";
 // import { MemoCounter } from "./06-memos/MemoCounter";
-import { InstagromApp } from "./07-useOptimistic/InstagromApp";
+// import { InstagromApp } from "./07-useOptimistic/InstagromApp";
 // import { TrafficLightWithHook } from "./02-useEffect/TrafficLightWithHook";
 // import { PokemonPage } from "./03-examples/PokemonPage";
 // import { FocusScreen } from "./04-useRef/FocusScreen";
@@ -25,6 +26,7 @@ createRoot(document.getElementById("root")!).render(
     {/* <ScrambleWords /> */}
     {/* <MemoHook/> */}
     {/* <MemoCounter/> */}
-    <InstagromApp />
+    {/* <InstagromApp /> */}
+    <ClientInformation id={123}/>
   </StrictMode>,
 );
